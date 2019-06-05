f65f37519fa157a09c8ce94a4e38a91c5f33dfd4
earthsittipong <earth32977@gmail.com>
2019-06-05 13:55:35 +0700

    first commit

---
+++ fizzbuzz.go
@@ -0,0 +1,26 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+//var i int = 100
+
+/* for loop number 1-100 */
+for i:=1;i<=100;i++{
+
+if i%15 == 0 {
+    fmt.Println("type of ",i,"=FizzBuzz")
+}else if i%5 == 0{
+    fmt.Println("type of ",i,"=Buzz")
+}else if i%3 == 0 {
+        fmt.Println("type of ",i,"=Fizz")
+} 
+
+
+ 
+
+}
+}
